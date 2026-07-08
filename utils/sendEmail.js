3261a5a3097d6100cb768ef41db9785af6a749ff
Rashika1810 <rashika.kr04@gmail.com>
2026-07-08 21:05:16 +0530

    changed mailer in function

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,30 +1,52 @@
+// const nodemailer = require("nodemailer");
+
+// const sendEmail = async (to, subject, html) => {
+//   const transporter = nodemailer.createTransport({
+//     host: "smtp.gmail.com",
+//     port: 465,
+//     secure: true,
+//     auth: {
+//       user: process.env.EMAIL_USER,
+//       pass: process.env.EMAIL_PASS,
+//     },
+//     connectionTimeout: 30000,
+//     greetingTimeout: 30000,
+//     socketTimeout: 30000,
+//   });
+
+//   await transporter.verify();
+//   console.log("SMTP Connected");
+
+//   await transporter.sendMail({
+//     from: `"Expense Tracker" <${process.env.EMAIL_USER}>`,
+//     to,
+//     subject,
+//     html,
+//   });
+
+//   console.log("Email sent successfully");
+// };
+
+// module.exports = sendEmail;
 const nodemailer = require("nodemailer");
 
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: Number(process.env.SMTP_PORT),
+  secure: false, // Port 587 uses STARTTLS
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
+
 const sendEmail = async (to, subject, html) => {
-  const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-    connectionTimeout: 30000,
-    greetingTimeout: 30000,
-    socketTimeout: 30000,
-  });
-
-  await transporter.verify();
-  console.log("SMTP Connected");
-
   await transporter.sendMail({
-    from: `"Expense Tracker" <${process.env.EMAIL_USER}>`,
+    from: `"Expense Tracker" <${process.env.MAIL_FROM}>`,
     to,
     subject,
     html,
   });
-
-  console.log("Email sent successfully");
 };
 
 module.exports = sendEmail;
